7f7736b1e31cbec4ed7914a54099a5801bf36649
iTh1nk <fnchao@hotmail.com>
2019-09-12 19:01:22 -0700

    replace all file with new

---
+++ app/public/js/map.js
@@ -0,0 +1,10 @@
+var mymap = L.map('mapid').setView([33.645, -117.835], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 16,
+  id: 'mapbox.streets',
+  accessToken: 'pk.eyJ1IjoidmlvbGlubWFuYSIsImEiOiJjazBlbXFqM3QwMWZ3M2lvYTVtMTBtdzZzIn0.25nYvC4e7xl4WXKLmKFqew'
+}).addTo(mymap);
+
+L.control.scale().addTo(mymap);
