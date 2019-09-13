87f6ecb5ae0dc8a16795e697a96be695787e2d87
iTh1nk <fnchao@hotmail.com>
2019-09-13 00:40:23 -0700

    save

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -4,59 +4,59 @@ var color = require("colors");
 var gloUser = "";
 
 exports.dashboard = function (req, res) {
-    db.post
-        .findAll({
-            include: [db.user]
-        })
-        .then((resultPost) => {
-            db.comment
-                .findAll({})
-                .then((resultComment) => {
-                    gloUser = req.user.name;
-                    res.render("success", {
-                        username: req.user.name,
-                        allPost: resultPost,
-                        allComment: resultComment
-                    });
-                    // res.json(resultComment);
-                })
-                .catch((err) => {
-                    console.log(err);
-                    res.status(400).json({ err: err, message: err.message });
-                });
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(400).json({ err: err, message: err.message });
-        });
+    // db.post
+    //     .findAll({
+    //         include: [db.user]
+    //     })
+    //     .then((resultPost) => {
+    //         db.comment
+    //             .findAll({})
+    //             .then((resultComment) => {
+    //                 gloUser = req.user.name;
+    //                 res.render("success", {
+    //                     username: req.user.name,
+    //                     allPost: resultPost,
+    //                     allComment: resultComment
+    //                 });
+    //                 // res.json(resultComment);
+    //             })
+    //             .catch((err) => {
+    //                 console.log(err);
+    //                 res.status(400).json({ err: err, message: err.message });
+    //             });
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //         res.status(400).json({ err: err, message: err.message });
+    //     });
 }
 
 exports.dashboardPost = function (req, res) {
-    var data = req.body;
-    console.log(data)
-    db.post
-        .create({ member_post: data.member_post, userId: req.user.id })
-        .then((result) => {
-            res.json(result);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(400).json({ err: err, message: err.message });
-        });
+    // var data = req.body;
+    // console.log(data)
+    // db.post
+    //     .create({ member_post: data.member_post, userId: req.user.id })
+    //     .then((result) => {
+    //         res.json(result);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //         res.status(400).json({ err: err, message: err.message });
+    //     });
 }
 
 exports.dashboardComment = function (req, res) {
-    var data = req.body;
-    console.log(data)
-    db.comment
-        .create({ member_comment: data.member_comment, postId: data.postId, member_name: gloUser})
-        .then((resultComment) => {
-            res.json(resultComment);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(400).json({ err: err, message: err.message });
-        });
+    // var data = req.body;
+    // console.log(data)
+    // db.comment
+    //     .create({ member_comment: data.member_comment, postId: data.postId, member_name: gloUser})
+    //     .then((resultComment) => {
+    //         res.json(resultComment);
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //         res.status(400).json({ err: err, message: err.message });
+    //     });
 }
 
 exports.logout = function (req, res) {
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -45,7 +45,6 @@ module.exports = function (app, passport) {
     function isLoggedIn(req, res, next) {
         if (req.isAuthenticated())
             return next();
-
         res.redirect('/');
     }
 
