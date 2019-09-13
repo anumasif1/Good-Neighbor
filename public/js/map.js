816a3c662e0073f581dac3c71ac71a75fabda973
Alex Hwong <violinmana@gmail.com>
2019-09-12 18:54:02 -0700

    more map stuff

--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,11 @@
-var mymap = L.map('mapid').setView([33.645, -117.835], 13);
 
+//var to replace marker coordinates with
+var latLong = [33.645, -117.835];
+
+//Need to replace setView to lat/long in database
+var mymap = L.map('mapid').setView(latLong, 13);
+
+//Mapbox API key + access token
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 16,
@@ -7,4 +13,11 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: 'pk.eyJ1IjoidmlvbGlubWFuYSIsImEiOiJjazBlbXFqM3QwMWZ3M2lvYTVtMTBtdzZzIn0.25nYvC4e7xl4WXKLmKFqew'
 }).addTo(mymap);
 
+//Add Scale to Map
 L.control.scale().addTo(mymap);
+
+//Add Pin to Map, replace marker with lat/long in database
+L.marker(latLong).addTo(mymap);
+
+//Add Circle with 1000 meters from middle point
+L.circle(latLong, {radius: 1000}).addTo(mymap);
