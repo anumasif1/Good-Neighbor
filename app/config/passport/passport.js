f292b3c1a38e09670a6c5794b135e1618991103d
iTh1nk <fnchao@hotmail.com>
2019-09-13 00:46:09 -0700

    save

--- app/config/passport/passport.js
+++ app/config/passport/passport.js
@@ -7,7 +7,7 @@ module.exports = function (passport, user) {
         done(null, user.id);
     });
     passport.deserializeUser(function (id, done) {
-        User.findByPk(id).then(function (user) {
+        User.findById(id).then(function (user) {
             if (user) {
                 done(null, user.get());
             }
