dc03742cd66716e54ced6ef7c1a5baaddfff9b6e
iTh1nk <fnchao@hotmail.com>
2019-09-13 21:44:46 -0700

    finishing touches

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ var db = require("./app/models");
 var authRoute = require("./app/routes/auth")(app, passport);
 require("./app/config/passport/passport")(passport, db.user);
 
-db.sequelize.sync({ force: false }).then(function () {
+db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
