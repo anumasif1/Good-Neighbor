12fd7bd61c4118d7ed7b678e0b7c1a53a04922be
iTh1nk <fnchao@hotmail.com>
2019-09-13 21:05:01 -0700

    fixed layout

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -35,7 +35,9 @@ module.exports = function (app, passport) {
     app.get('/sunshinejpg/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/styles/images/sunshine.jpg'));
     })
-
+    app.get('/mapjs/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/assets/js/map.js'));
+    })
     
     app.use('', function (req, res) {
         // res.sendFile(path.join(__dirname, '../public/assets/img/404.png'));
